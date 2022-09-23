0677bc6ff22bf7bd7fd417e367aeea5f8441efc5
Pheerztao <faadedokun8@gmail.com>
2022-09-23 10:39:55 +0100

    node project

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require(`express`)
+const app = express()
+const bodyParser = require(`body-parser`)
+const port = 5000
+
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+
+  console.log(`The server works for alora : ${port}`)
+  
+})
+
+//localhost: 5000/alora => alora, where are you
+//localhost: 5000/lily/retard => dream, face reveal
+
+app.get(`/alora`, (req, res) => {
+
+     res.send(`alora, where are you`)
+
+})
+
+app.get(`/today`, (req, res) => {
+  let today = new Date()
+  res.send(today)
+  
+})
+//JSON concept
+app.get(`/lily/retard`, (req, res) => {
+
+  res.send({
+       message: "hello, resert.."
+  })
+
+})
+
+app.get(`/sammy/leed`, (req, res) => {
+    const data = req.body 
+  res.send({
+       message: "welcome, leader.."
+  })
+
+})
+
+app.post(`/create`, (req, res) => {
+  const data = req.body 
+  console.log(data)
+  if(data.fn == "" || data.sn == "" || data.email == ""){
+    res.send({
+      message: "All fields are required"
+ })
+  
+}else{
+  res.send({
+    message: `User information accepted, Welcome User ${data.sn}, ${data.fn}, ${data.email}`
+})
+}
+
+})
